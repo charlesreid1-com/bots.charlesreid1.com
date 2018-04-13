3cb155a9a736fbba0b406e4eb0e4ec0af4e82781
Charles Reid <charlesreid1@gmail.com>
2018-04-13 04:31:15 -0700

    add initial version of bots site with variables defined

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -21,7 +21,72 @@ THEME = 'scurvy-knave-theme'
 # Pelican is designed for files => pages.
 # Use variables (below) to set pieces of pages.
 
-CTHULHU = "Cthulhu R'lyeh fhtagn"
+# ---
+
+# description appears between <p> tags, so don't include them
+
+SITE_TITLE = "Cthulhu fhtagn"
+
+SITE_DESCRIPTION = """Ph'nglui mglw'nafh Cthulhu R'lyeh wgah' fhtagn.<br />
+Haflorr'e Cthulhuagl epog ep, 'aiagl lw'nafh mnahn' nalw'nafh<br />
+n'gha gof'nn. Tharanak f's'uhnee Chaugnar shogg Nyarlathotepyar."""
+
+# ---
+
+# include <p> tags in the description
+
+ABOUT_TITLE = "About Cthulhu R'lyeh"
+
+ABOUT_DESCRIPTION = """<p class="lead">Lloig nnnron k'yarnak Dagon Chaugnar Faugn fm'latgh shogg.</p>
+
+<p>Stell'bsna Dagon orr'e stell'bsna nnnlw'nafh wgah'n grah'n, Hastur.<br />
+'bthnk ee cnw bug ehye. Shub-niggurath Tsathoggua Nyarlathotep y-syha'h.<p/>
+
+<p>Azathoth R'lyeh orr'e phlegeth grah'n n'ghft, y'hah lw'nafh sll'ha lw'nafh.<br />
+Wgah'n Nyarlathotep f'gof'nn, gotha hupadgh nog r'luhog athgoth ehye ep uh'e.</p>"""
+
+# ---
+
+# include <p> tags in the description
+
+LINKS_TITLE = "Download Cthulhu Tsathoggua"
+
+LINKS_DESCRIPTION = """
+<p>Kadishtu n'ghft gof'nn fm'latgh Azathoth lloig chtenff hafh'drn shoggyar.</p>
+<p>Ya nog Azathoth ah R'lyeh Chaugnar hupadgh kn'a hlirgh Nyarlathotep.</p>"""
+
+FLOCK_URL = "https://twitter.com/charlesreid1/lists/miltonbotflock"
+
+LINKS_BUTTONS = """<p><a href="%s" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> Bot Flock</a></p>
+<p>
+<a href="https://twitter.com/milton_book1"  class="btn btn-default btn-lg">@milton_book1</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book2"  class="btn btn-default btn-lg">@milton_book2</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book3"  class="btn btn-default btn-lg">@milton_book3</a>
+</p><p>
+<a href="https://twitter.com/milton_book4"  class="btn btn-default btn-lg">@milton_book4</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book5"  class="btn btn-default btn-lg">@milton_book5</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book6"  class="btn btn-default btn-lg">@milton_book6</a>
+</p><p>
+<a href="https://twitter.com/milton_book7"  class="btn btn-default btn-lg">@milton_book7</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book8"  class="btn btn-default btn-lg">@milton_book8</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book9"  class="btn btn-default btn-lg">@milton_book9</a>
+</p><p>
+<a href="https://twitter.com/milton_book10" class="btn btn-default btn-lg">@milton_book10</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book11" class="btn btn-default btn-lg">@milton_book11</a> &nbsp; &nbsp; &nbsp; 
+<a href="https://twitter.com/milton_book12" class="btn btn-default btn-lg">@milton_book12</a>
+</p>
+"""%(FLOCK_URL)
+
+
+# ---
+
+
+CONTACT_TITLE = "Contact Cthulhu"
+
+CONTACT_DESCRIPTION = """<p>Get in touch!</p>
+<p><a href="mailto:charles@charlesreid1.com">charles@charlesreid1.com</a></p>
+"""
+
 
 # --------------8<---------------------
 
